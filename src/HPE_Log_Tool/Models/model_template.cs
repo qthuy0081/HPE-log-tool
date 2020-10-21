f15032cdad65130b0a098c875d7294e88f63f9cc
qthuy <qthuy.0081@gmail.com>
2020-10-21 13:45:46 +0700

    create folder structure

---
+++ src/HPE_Log_Tool/Models/model_template.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPE_Log_Tool.Models
+{
+    class model_template
+    {
+    }
+}
---
+++ src/HPE_Log_Tool/ViewModels/viewmodel_template.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPE_Log_Tool.ViewModels
+{
+    class viewmodel_template
+    {
+    }
+}
